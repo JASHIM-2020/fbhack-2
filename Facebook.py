8952b7f9af22bfadd9ebdb9529de51e244f90cc5
Arkar Kyaw <47052658+arkarkyaw2138@users.noreply.github.com>
2019-11-30 21:36:01 +0630

    Update Facebook.py

--- Facebook.py
+++ Facebook.py
@@ -1,6 +1,7 @@
-import time
 import sys
 import os
+import time
+#Code Time
 from datetime import datetime
 now = datetime.now()
 hour = now.hour
@@ -9,36 +10,41 @@ day = now.day
 month = now.month
 year = now.year
 
-
-os system("clear")
-
-os.system("figlet Min May Sout Phote Facebook Hack")
-
-
-os.system("figlet Ngar Loe Ma That Tway")
-
+os.system("apt install figlet")
+os.system("clear")
+os.system("figlet Phin Khan Like")
+time.sleep(5)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 0%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 10%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 20%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 30%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 40%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 50%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 60%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 70%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 80%---''')
-if sys.version_info[0] !=2: 
+time.sleep(3)
+if sys.version_info[0] !=2:
 	print('''---Hack Min May Sout Phote 90%---''')
+time.sleep(3)
 if sys.version_info[0] !=2: 
 	print('''---Hack Min May Sout Phote 100%---''')
-if sys.version_info[0] !=2: 
-	print('''---Min May Sout Phote is Hacked By S!l3NT_680UL---''') 
-    
+if sys.version_info[0] !=2:
+	print('''Min May Sout Phote is Hacked By S!l3NT_680UL---''') 
