40ce293ac18929972bcfa9f9ff2aebd7cef08338
Arkar Kyaw <47052658+arkarkyaw2138@users.noreply.github.com>
2019-11-30 15:26:50 +0630

    fb hack
    Hack For Fàcebook

---
+++ hack.py
@@ -0,0 +1,28 @@
+import time
+import sys
+
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 0%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 10%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 20%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 30%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 40%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 50%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 60%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 70%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 80%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 90%---''')
+if sys.version_info[0] !=2: 
+	print('''---Hack Min May Sout Phote 100%---''')
+if sys.version_info[0] !=2: 
+	print('''---Min May Sout Phote is Hacked By S!l3NT_680UL---''') 
+    
